ec42111ad3142f4dfdb26d23e657cdcdf80c5b1d
Maxime Quandalle <maxime.quandalle@gmail.com>
2014-09-05 15:46:57 +0200

    Update match tests for Meteor 0.9.1 see https://github.com/meteor/meteor/issues/2523

--- tests/match.js
+++ tests/match.js
@@ -5,21 +5,22 @@ var removeLineComment = function (code) {
   }).join(lineBreak);
 };
 
-var tpl2txt = function(tpl) {
-  return tpl.__render && removeLineComment(tpl.__render.toString());
+var tpl2txt = function(tplName) {
+  var tpl = Template[tplName];
+  return tpl.renderFunction && removeLineComment(tpl.renderFunction.toString());
 };
 
 Tinytest.add('Jade - Compiled template match Spacebars', function (test) {
-  _.each(Template, function(jadeTpl, tplName) {
-    if (! tplName.match(/^match-jade/))
-      return;
+  for (var jadeTplName in Template) {
+    if (! jadeTplName.match(/^match-jade/))
+      continue;
 
-    var testName = tplName.split('-')[2];
+    var testName = jadeTplName.split('-')[2];
     var htmlTplName = ["match", "html", testName].join("-");
 
     if (_.has(Template, htmlTplName))
-      test.equal(tpl2txt(jadeTpl), tpl2txt(Template[htmlTplName]), testName);
+      test.equal(tpl2txt(jadeTplName), tpl2txt(htmlTplName), testName);
     else
       test.fail("Missing template: " + htmlTplName);
-  });
+  }
 });
