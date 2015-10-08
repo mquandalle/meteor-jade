236b20ea88b3628364323c4a135d334585948a01
Darryn Ten <darryn@nonacreative.com>
2015-10-08 10:41:44 +0200

    Meteor 1.2 Compatibility
    Explicit targets when including packages and files (`server`, `client`, etc).
    Resolves #168 and possibly #169
    
    Minimum Meteor version bumped from `0.9.0` to `1.2.0.1`. Meteor 1.2 has breaking
    changes so this is not compatible with versions lower than this. I'm not sure
    how backwards compatibility should be handled.
    
    Updated `versions.json` files with versions required by Meteor 1.2
    
    Removed hardcoded versions in the build plugin. Rationale is to use the versions
    used by Meteor 1.2 or versions set by the developer.
    
    Added `underscore` and `jquery` to the tests as these are no longer global.
    
    Bumped package version up to `0.4.4`

--- packages/jade-compiler/package.js
+++ packages/jade-compiler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Compiler for the meteor-jade template language",
-  version: "0.4.3",
+  version: "0.4.4",
   name: "mquandalle:jade-compiler",
   git: "https://github.com/mquandalle/meteor-jade.git",
   documentation: "../../README.md"
@@ -11,26 +11,25 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom("METEOR@0.9.0");
+  api.versionsFrom("METEOR@1.2.0.1");
   api.use([
     'underscore',
-    'htmljs',
     'html-tools',
     'blaze-tools',
     'spacebars-compiler'
-  ]);
+  ], ['server']);
   api.use('minifiers', ['server'], { weak: true });
   api.addFiles([
     'lib/lexer.js',
     'lib/parser.js',
     'lib/transpilers.js',
     'lib/exports.js'
-  ]);
+  ], ['server']);
   api.export('JadeCompiler');
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom("METEOR@0.9.0");
+  api.versionsFrom("METEOR@1.2.0.1");
   api.use("tinytest");
   api.use("minifiers");
   api.use("mquandalle:jade-compiler", "server");
