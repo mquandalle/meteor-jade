497d3294e02d08505fc2f34620a839611ce9ccca
Kristian Dalgård <k.dalgard@gmail.com>
2015-10-12 03:02:00 +0200

    Created example

---
+++ examples/viewmodel/client/lib/helpers.js
@@ -0,0 +1,10 @@
+Template.registerHelper("name", numeral => "Name " + numeral);
+
+Template.registerHelper("person", (name, kw) => {
+  let intro = "Hi, my name is " + name;
+
+  if (kw && kw.hash)
+    intro += " and my favorite color is " + kw.hash.color;
+
+  return intro;
+});
---
+++ examples/viewmodel/client/lib/register-bind.js
@@ -0,0 +1,1 @@
+ViewModel.registerHelper("bind");
